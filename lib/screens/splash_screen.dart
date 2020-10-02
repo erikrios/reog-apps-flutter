046579e6ee362caac3a28cdf5c4e02d798818050
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-02 09:53:47 +0700

    Add splash screen logo

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -9,6 +10,8 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
+    final SplashScreenLogo splashScreenLogo = SplashScreenLogo();
+
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
@@ -16,13 +19,24 @@ class _SplashScreenState extends State<SplashScreen> {
       child: Scaffold(
         backgroundColor: Color(0xff97DA7B),
         body: Center(
-          child: Text(
-            'Hello, World!',
-            textAlign: TextAlign.center,
-            style: TextStyle(decoration: TextDecoration.none),
+          child: Padding(
+            padding: const EdgeInsets.all(58.0),
+            child: splashScreenLogo,
           ),
         ),
       ),
     );
+  }
+}
+
+class SplashScreenLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String asset = 'assets/peacock.svg';
+    final Widget logo = SvgPicture.asset(
+      asset,
+      semanticsLabel: 'Reog Apps Logo',
+    );
+    return logo;
   }
 }
