0e747c9a63a478ab95698b012846a1cc08d31e30
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-03 12:19:22 +0700

    Clean the code

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'dart:async';
 import 'package:reog_apps_flutter/screens/dashboard.dart';
