1568b159d5df0ddbc72fa8725f9b179a720bc913
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-14 10:18:08 +0700

    Create login page

--- lib/src/screens/pages/login_page.dart
+++ lib/src/screens/pages/login_page.dart
@@ -1,164 +1,118 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:reog_apps_flutter/src/screens/widgets/form_field_item.dart';
 
 class LoginPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => new _State();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _State extends State<LoginPage> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Color(0xff97DA7B),
-        body: Container(
-          padding: EdgeInsets.symmetric(vertical: 70, horizontal: 30),
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.green[700], Colors.lightGreen[300]],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
           child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Container(
-                  height: 160,
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'login'.tr().toUpperCase(),
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
-                        color: Colors.white),
-                  )),
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'email_address'.tr(),
-                      textAlign: TextAlign.start,
-                      style: TextStyle(fontSize: 25, color: Colors.white),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      child: TextFormField(
-                        textInputAction: TextInputAction.next,
-                        onFieldSubmitted: (_) =>
-                            FocusScope.of(context).nextFocus(),
-                        validator: (String value) {
-                          return value.isEmpty ? 'email_hint'.tr() : null;
-                        },
-                        controller: nameController,
-                        decoration: InputDecoration(
-                          fillColor: Colors.white,
-                          filled: true,
-                          border: OutlineInputBorder(),
-                          hintText: 'email_hint'.tr().toString(),
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                        onSaved: (String value) {
-                          // This optional block of code can be used to run
-                          // code when the user saves the form.
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'password'.tr(),
-                      textAlign: TextAlign.start,
-                      style: TextStyle(fontSize: 25, color: Colors.white),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      textInputAction: TextInputAction.next,
-                      onFieldSubmitted: (_) =>
-                          FocusScope.of(context).nextFocus(),
-                      validator: (String value) {
-                        return value.isEmpty ? 'password_hint'.tr() : null;
-                      },
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: OutlineInputBorder(),
-                        hintText: 'password_hint'.tr(),
-                      ),
-                      keyboardType: TextInputType.visiblePassword,
-                      onSaved: (String value) {
-                        // This optional block of code can be used to run
-                        // code when the user saves the form.
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 60,
-              ),
-              Container(
-                height: 50,
-                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  textColor: Colors.white,
-                  color: Colors.yellow.shade700,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 60),
-                    child: Text('login'.toUpperCase(),
+            children: [
+              Flexible(
+                  flex: 1,
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 28),
+                      child: Text(
+                        'login'.tr(),
                         style: TextStyle(
-                            fontWeight: FontWeight.w900, fontSize: 20)),
+                            color: Colors.white,
+                            fontSize: 48,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )),
+              Flexible(
+                flex: 4,
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(left: 28, right: 28),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      FormFieldItem('email_address'.tr(), 'email_hint'.tr(),
+                          false, emailController),
+                      FormFieldItem('password'.tr(), 'password_hint'.tr(), true,
+                          passwordController),
+                      Center(
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width / 2,
+                          child: RaisedButton(
+                            padding: EdgeInsets.only(top: 8, bottom: 8),
+                            color: Color(0xffE6CB34),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            onPressed: () {
+                              print(emailController.text);
+                              print(passwordController.text);
+                            },
+                            child: Text(
+                              'login'.tr().toUpperCase(),
+                              style:
+                                  TextStyle(fontSize: 26, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
                 ),
               ),
-              Spacer(
-                flex: 2,
-              ),
-              Container(
-                  child: Row(
-                children: <Widget>[
-                  Text(
-                    'have_not_account'.tr(),
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  FlatButton(
-                    textColor: Colors.blue,
-                    child: Text(
-                      'register'.tr(),
-                      style: TextStyle(
-                          color: Colors.yellow,
-                          fontSize: 20,
-                          decoration: TextDecoration.underline),
+              Flexible(
+                flex: 1,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'have_not_account'.tr(),
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        SizedBox(width: 4),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Text(
+                            'register'.tr(),
+                            style: TextStyle(
+                              color: Colors.yellow,
+                              fontSize: 16,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                    onPressed: () {
-                      //signup screen
-                    },
-                  )
-                ],
-                mainAxisAlignment: MainAxisAlignment.center,
-              ))
+                  ),
+                ),
+              )
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
