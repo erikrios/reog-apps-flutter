a6148239c63b113901bd5956169963e3903e9d12
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-14 10:17:51 +0700

    Create form_field_item.dart

---
+++ lib/src/screens/widgets/form_field_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class FormFieldItem extends StatelessWidget {
+  final String header;
+  final String hint;
+  final bool obsecureText;
+  final TextEditingController controller;
+
+  FormFieldItem(this.header, this.hint, this.obsecureText, this.controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          header,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        SizedBox(height: 10),
+        TextField(
+          controller: controller,
+          obscureText: obsecureText,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0xffEBEBEB),
+            hintText: hint,
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide.none),
+          ),
+        ),
+      ],
+    );
+  }
+}
