0562593a5f17b04cc260bd360a7f5afeced581c8
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-14 10:59:41 +0700

    Create register page

---
+++ lib/src/screens/pages/register_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:reog_apps_flutter/src/screens/widgets/form_field_item.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController fullNameController = new TextEditingController();
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+  final TextEditingController rePasswordController =
+      new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.green[700], Colors.lightGreen[300]],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 28),
+                  child: Text(
+                    'register'.tr(),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 5,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(left: 28, right: 28),
+                child: ListView(
+                  children: [
+                    FormFieldItem('full_name'.tr(), 'full_name_hint'.tr(),
+                        false, fullNameController),
+                    SizedBox(height: 4),
+                    FormFieldItem('email_address'.tr(), 'email_hint'.tr(), false,
+                        emailController),
+                    SizedBox(height: 4),
+                    FormFieldItem('password'.tr(), 'password_hint'.tr(), true,
+                        passwordController),
+                    SizedBox(height: 4),
+                    FormFieldItem('re_password'.tr(), 're_password_hint'.tr(),
+                        true, rePasswordController),
+                  ],
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width / 2,
+                  child: RaisedButton(
+                    padding: EdgeInsets.only(top: 8, bottom: 8),
+                    color: Color(0xffE6CB34),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'register'.tr().toUpperCase(),
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
