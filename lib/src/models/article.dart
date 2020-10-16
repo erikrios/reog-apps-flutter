fd688220c54a25dd593cd07e19dd419b85c0ef26
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-16 21:01:15 +0700

    Destructuring the article model class

--- lib/src/models/article.dart
+++ lib/src/models/article.dart
@@ -1,17 +1,32 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:reog_apps_flutter/src/models/image.dart';
+
+part 'article.g.dart';
+
+@JsonSerializable()
 class Article {
-  String _id;
-  String _title;
-  String _description;
-  String _date;
-  int _views;
-  List<String> _images;
+  @JsonKey(name: '_id')
+  String id;
+  @JsonKey(name: 'title')
+  String title;
+  @JsonKey(name: 'description')
+  String description;
+  @JsonKey(name: 'date')
+  String date;
+  @JsonKey(name: 'views')
+  int views;
+  @JsonKey(name: 'images')
+  List<Image> images;
 
-  Article(this._id, this._title, this._description, this._date, this._views,
-      this._images);
+  Article(
+      {this.id,
+      this.title,
+      this.description,
+      this.date,
+      this.views,
+      this.images});
 
-  String get title => _title;
-  String get description => _description;
-  String get date => _date;
-  int get views => _views;
-  List<String> get images => _images;
+  factory Article.fromJson(Map<String, dynamic> json) => _$ArticleFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ArticleToJson(this);
 }
