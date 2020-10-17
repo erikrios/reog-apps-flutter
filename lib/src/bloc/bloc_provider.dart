9935349d54118454666279ac29bc307cdfeac26d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-17 18:46:13 +0700

    Crate bloc_provider.dart

---
+++ lib/src/bloc/bloc_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:reog_apps_flutter/src/bloc/bloc.dart';
+
+class BlocProvider<T extends Bloc> extends StatefulWidget {
+  final Widget child;
+  final T bloc;
+
+  const BlocProvider({Key key, @required this.bloc, @required this.child})
+      : super(key: key);
+
+  static T of<T extends Bloc>(BuildContext context) {
+    final BlocProvider<T> provider = context.findAncestorWidgetOfExactType();
+    return provider.bloc;
+  }
+
+  @override
+  State createState() => _BlocProviderState();
+}
+
+class _BlocProviderState extends State<BlocProvider> {
+  @override
+  Widget build(BuildContext context) => widget.child;
+
+  @override
+  void dispose() {
+    widget.bloc.dispose();
+    super.dispose();
+  }
+}
