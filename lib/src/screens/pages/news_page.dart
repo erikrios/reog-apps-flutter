af85ae8f5962b854c6fb15534b87ad452253e18f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-22 20:36:30 +0700

    Refactor the code

--- lib/src/screens/pages/news_page.dart
+++ lib/src/screens/pages/news_page.dart
@@ -1,11 +1,9 @@
-import 'dart:convert';
-
-import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
-import 'package:reog_apps_flutter/src/bloc/bloc_provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reog_apps_flutter/src/bloc/events/news_result_event.dart';
 import 'package:reog_apps_flutter/src/bloc/news_result_bloc.dart';
+import 'package:reog_apps_flutter/src/bloc/states/news_result_state.dart';
 import 'package:reog_apps_flutter/src/models/article.dart';
-import 'package:reog_apps_flutter/src/models/news.dart';
 import 'package:reog_apps_flutter/src/models/news_result.dart';
 import 'package:reog_apps_flutter/src/screens/pages/details_page.dart';
 import 'package:reog_apps_flutter/src/screens/widgets/article_item.dart';
@@ -14,57 +12,59 @@ class NewsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = BlocProvider.of<NewsResultBloc>(context);
-    bloc.getNews();
-    return _buldStreamBuilder(bloc);
+    bloc.add(NewsResultFetching());
+
+    return _buildBlocBuilder(bloc);
   }
 
-  StreamBuilder _buldStreamBuilder(NewsResultBloc bloc) {
-    return StreamBuilder<Response>(
-        stream: bloc.stream,
-        builder: (context, snapshot) {
-          print(snapshot.hasData);
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            final results = NewsResult.fromJson(jsonDecode(snapshot.data.body));
-            if (results.status.contains('error')) {
-              return Center(
-                child: Text(results.message),
-              );
-            } else if (results.data[0].news.isEmpty) {
-              return Center(
-                child: Text('News is empty'),
-              );
-            } else {
-              return _buildNewsResults(results.data[0]);
-            }
-          }
-        });
+  Widget _buildBlocBuilder(NewsResultBloc bloc) {
+    return BlocBuilder<NewsResultBloc, NewsResultState>(
+      cubit: bloc,
+      builder: (BuildContext context, NewsResultState state) {
+        return state is NewsResultLoadingState
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : state is NewsResultErrorState
+                ? Center(
+                    child: Text(state.error),
+                  )
+                : _buildNewsResults(
+                    (state as NewsResultSuccessState).newsResult);
+      },
+    );
   }
 
-  Container _buildNewsResults(News news) {
+  Widget _buildNewsResults(NewsResult newsResult) {
+    final status = newsResult.status;
+    final news = newsResult.data[0];
+
     return Container(
-      child: ListView.builder(
-        padding: EdgeInsets.all(12.0),
-        itemCount: news.news.length,
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            child: ArticleItem(
-              image: news.news[index].images[0].image,
-              title: news.news[index].title,
-              date: news.news[index].date,
-              description: news.news[index].description,
-            ),
-            onTap: () {
-              _navigateToDetails(
-                context: context,
-              );
-            },
-          );
-        },
-      ),
+      child: status.toLowerCase() == 'error'
+          ? Center(child: Text(newsResult.message))
+          : news.news.isEmpty
+              ? Center(
+                  child: Text('News is empty.'),
+                )
+              : ListView.builder(
+                  padding: EdgeInsets.all(12.0),
+                  itemCount: news.news.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      child: ArticleItem(
+                        image: news.news[index].images[0].image,
+                        title: news.news[index].title,
+                        date: news.news[index].date,
+                        description: news.news[index].description,
+                      ),
+                      onTap: () {
+                        _navigateToDetails(
+                          context: context,
+                        );
+                      },
+                    );
+                  },
+                ),
     );
   }
 
