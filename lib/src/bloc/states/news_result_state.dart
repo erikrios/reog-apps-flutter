2f2de00df6f62b532401a7f6ab4a60c200e69344
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-22 18:41:01 +0700

    Create NewsResultSuccessState class

---
+++ lib/src/bloc/states/news_result_state.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+import 'package:reog_apps_flutter/src/models/news_result.dart';
+
+abstract class NewsResultState extends Equatable {
+  const NewsResultState();
+}
+
+class NewsResultLoadingState extends NewsResultState {
+  @override
+  List<Object> get props => [];
+}
+
+class NewsResultSuccessState extends NewsResultState {
+  final NewsResult newsResult;
+
+  NewsResultSuccessState({this.newsResult});
+
+  @override
+  List<Object> get props => [newsResult];
+}
