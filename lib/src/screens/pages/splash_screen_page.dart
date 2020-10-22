47da2db76c8bbdf6e97973c934d96e555df630e2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-22 19:49:48 +0700

    Refactory the code

--- lib/src/screens/pages/splash_screen_page.dart
+++ lib/src/screens/pages/splash_screen_page.dart
@@ -1,33 +1,14 @@
+import 'dart:async';
+
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:reog_apps_flutter/src/screens/pages/dashboard_page.dart';
-import 'dart:async';
-import 'package:easy_localization/easy_localization.dart';
 
-class SplashScreenPage extends StatefulWidget {
-  @override
-  _SplashScreenPageState createState() => _SplashScreenPageState();
-}
-
-class _SplashScreenPageState extends State<SplashScreenPage> {
-  @override
-  void initState() {
-    super.initState();
-    _startSplashScreen();
-  }
-
-  _startSplashScreen() async {
-    final duration = Duration(milliseconds: 1500);
-    return Timer(duration, () {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
-        return DashboardPage();
-      }));
-    });
-  }
-
+class SplashScreenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final SplashScreenLogo splashScreenLogo = SplashScreenLogo();
+    _startSplashScreen(context);
 
     return Scaffold(
       backgroundColor: Color(0xff97DA7B),
@@ -37,7 +18,7 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
             child: Center(
               child: Padding(
                 padding: const EdgeInsets.all(50.0),
-                child: splashScreenLogo,
+                child: _getSplashScreenLogo(),
               ),
             ),
           ),
@@ -60,16 +41,23 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
       ),
     );
   }
-}
 
-class SplashScreenLogo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+  _startSplashScreen(BuildContext context) async {
+    final duration = Duration(milliseconds: 1500);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
+        return DashboardPage();
+      }));
+    });
+  }
+
+  Widget _getSplashScreenLogo() {
     final String asset = 'assets/peacock.svg';
     final Widget logo = SvgPicture.asset(
       asset,
       semanticsLabel: 'Reog Apps Logo',
     );
+
     return logo;
   }
 }
