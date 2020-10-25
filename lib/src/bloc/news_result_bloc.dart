b01261e986467fa194031fc78ab2dd71816e38f3
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-25 21:38:17 +0700

    Add more logic

--- lib/src/bloc/news_result_bloc.dart
+++ lib/src/bloc/news_result_bloc.dart
@@ -5,11 +5,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reog_apps_flutter/src/bloc/events/news_result_event.dart';
 import 'package:reog_apps_flutter/src/bloc/states/news_result_state.dart';
+import 'package:reog_apps_flutter/src/models/article.dart';
 import 'package:reog_apps_flutter/src/models/news_result.dart';
 import 'package:reog_apps_flutter/src/service/reog_apps_service.dart';
 
 class NewsResultBloc extends Bloc<NewsResultEvent, NewsResultState> {
   final ReogAppsService service;
+  final List<Article> news = [];
 
   NewsResultBloc({@required this.service})
       : assert(service != null),
@@ -23,11 +25,22 @@ class NewsResultBloc extends Bloc<NewsResultEvent, NewsResultState> {
         Response response =
             await service.getNews(page: event.page, limit: event.limit);
         final newsResult = NewsResult.fromJson(jsonDecode(response.body));
+        news.addAll(newsResult.data[0].news);
         yield NewsResultSuccessState(newsResult: newsResult);
       } catch (e) {
-        print(e);
         yield NewsResultErrorState(error: e.toString());
       }
+    } else if (event is NewsResultFetchingMore) {
+      yield NewsResultLoadingMoreState();
+      try {
+        Response response =
+            await service.getNews(page: event.page, limit: event.limit);
+        final newsResult = NewsResult.fromJson(jsonDecode(response.bodyString));
+        news.addAll(newsResult.data[0].news);
+        newsResult.data[0].news.clear();
+        newsResult.data[0].news.addAll(news);
+        yield NewsResultSuccessState(newsResult: newsResult);
+      } catch (e) {}
     }
   }
 }
