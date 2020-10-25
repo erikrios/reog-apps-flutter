19641350f97ffe28f3bf250420f3d061054265db
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-25 23:12:24 +0700

    Change the pagination logic

--- lib/src/screens/pages/news_page.dart
+++ lib/src/screens/pages/news_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
 import 'package:reog_apps_flutter/src/bloc/events/news_result_event.dart';
 import 'package:reog_apps_flutter/src/bloc/news_result_bloc.dart';
 import 'package:reog_apps_flutter/src/bloc/states/news_result_state.dart';
 import 'package:reog_apps_flutter/src/models/article.dart';
-import 'package:reog_apps_flutter/src/models/news_result.dart';
+import 'package:reog_apps_flutter/src/models/news.dart';
 import 'package:reog_apps_flutter/src/screens/pages/details_page.dart';
 import 'package:reog_apps_flutter/src/screens/widgets/article_item.dart';
 
@@ -14,28 +15,22 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
-  NewsResultBloc bloc;
-  final ScrollController _controller = ScrollController();
+  NewsResultBloc _bloc;
   int _currentPage = 1;
   int _totalPages = 1;
+  final int _limit = 5;
+  String status;
+  News news;
 
   @override
   void initState() {
     super.initState();
-    bloc = BlocProvider.of<NewsResultBloc>(context);
+    _bloc = BlocProvider.of<NewsResultBloc>(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    // _controller.addListener(() {
-    //   if (_controller.position.pixels == _controller.position.maxScrollExtent) {
-    //     if (_currentPage <= _totalPages) {
-    //       print('Last');
-    //     }
-    //   }
-    // });
-
-    return _buildBlocBuilder(bloc);
+    return _buildBlocBuilder(_bloc);
   }
 
   Widget _buildBlocBuilder(NewsResultBloc bloc) {
@@ -43,54 +38,77 @@ class _NewsPageState extends State<NewsPage> {
         cubit: bloc,
         builder: (BuildContext context, NewsResultState state) {
           if (state is NewsResultInitialState) {
-            bloc.add(NewsResultFetching(page: _currentPage, limit: 5));
-            return Center(child: CircularProgressIndicator());
+            bloc.add(NewsResultFetching(page: _currentPage, limit: _limit));
+            return Container();
           } else if (state is NewsResultLoadingState) {
             return Center(child: CircularProgressIndicator());
           } else if (state is NewsResultErrorState) {
             return Center(child: Text(state.error));
           } else {
-            return _buildNewsResults(
-                (state as NewsResultSuccessState).newsResult);
+            return _buildNewsResults(state);
           }
         });
   }
 
-  Widget _buildNewsResults(NewsResult newsResult) {
-    final status = newsResult.status;
-    final news = newsResult.data[0];
-    _currentPage = newsResult.data[0].currentPage;
-    _totalPages = newsResult.data[0].totalPages;
-
+  Widget _buildNewsResults(NewsResultState state) {
+    if (state is NewsResultSuccessState) {
+      status = state.newsResult.status;
+      news = state.newsResult.data[0];
+      _currentPage = state.newsResult.data[0].currentPage;
+      _totalPages = state.newsResult.data[0].totalPages;
+    }
     return Container(
-      child: status.toLowerCase() == 'error'
-          ? Center(child: Text(newsResult.message))
-          : news.news.isEmpty
-              ? Center(
-                  child: Text('News is empty.'),
-                )
-              : ListView.builder(
-                  padding: EdgeInsets.all(12.0),
-                  itemCount: news.news.length,
-                  controller: _controller,
-                  itemBuilder: (BuildContext context, int index) {
-                    return InkWell(
-                      child: ArticleItem(
-                        image: news.news[index].images.isEmpty
-                            ? ""
-                            : news.news[index].images[0],
-                        title: news.news[index].title ?? "",
-                        date: news.news[index].date ?? "",
-                        description: news.news[index].description ?? "",
-                      ),
-                      onTap: () {
-                        _navigateToDetails(
-                            context: context, article: news.news[index]);
-                      },
-                    );
-                  },
-                ),
-    );
+        child: status.toLowerCase() == 'error'
+            ? Center(
+                child:
+                    Text((state as NewsResultSuccessState).newsResult.message))
+            : news.news.isEmpty
+                ? Center(
+                    child: Text('News is empty.'),
+                  )
+                : LazyLoadScrollView(
+                    onEndOfPage: _loadMoreData,
+                    child: ListView(
+                      padding: EdgeInsets.all(0),
+                      children: [
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          padding: EdgeInsets.all(12.0),
+                          itemCount: news.news.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return InkWell(
+                              child: ArticleItem(
+                                image: news.news[index].images.isEmpty
+                                    ? ""
+                                    : news.news[index].images[0],
+                                title: news.news[index].title ?? "",
+                                date: news.news[index].date ?? "",
+                                description: news.news[index].description ?? "",
+                              ),
+                              onTap: () {
+                                _navigateToDetails(
+                                    context: context,
+                                    article: news.news[index]);
+                              },
+                            );
+                          },
+                        ),
+                        (state is NewsResultLoadingMoreState)
+                            ? Center(
+                                child: CircularProgressIndicator(),
+                              )
+                            : SizedBox()
+                      ],
+                    ),
+                  ));
+  }
+
+  void _loadMoreData() {
+    if (_currentPage < _totalPages) {
+      _currentPage++;
+      _bloc.add(NewsResultFetchingMore(page: _currentPage, limit: _limit));
+    }
   }
 
   void _navigateToDetails(
