aa84b968da84c0464c35c5f994712cc56b63c166
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-29 13:24:23 +0700

    Implements the bloc

--- lib/src/screens/pages/details_page.dart
+++ lib/src/screens/pages/details_page.dart
@@ -1,68 +1,86 @@
-
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reog_apps_flutter/src/bloc/article_details_result_bloc.dart';
+import 'package:reog_apps_flutter/src/bloc/events/article_details_result_event.dart';
+import 'package:reog_apps_flutter/src/bloc/states/article_details_result_state.dart';
 import 'package:reog_apps_flutter/src/models/article.dart';
 import 'package:reog_apps_flutter/src/models/comment.dart';
 import 'package:reog_apps_flutter/src/screens/widgets/comment_item.dart';
 import 'package:reog_apps_flutter/src/screens/widgets/main_pop_up_menu.dart';
+import 'package:reog_apps_flutter/src/service/reog_apps_service.dart';
+import 'package:reog_apps_flutter/src/utils/article_type.dart';
 
 class DetailsPage extends StatefulWidget {
   final Article _article;
+  final ArticleType _type;
 
-  DetailsPage(this._article);
+  DetailsPage(this._article, this._type);
 
   @override
-  _DetailsPageState createState() => _DetailsPageState(_article);
+  _DetailsPageState createState() => _DetailsPageState(_article, _type);
 }
 
 class _DetailsPageState extends State<DetailsPage> {
   ScrollController _scrollViewController;
   final Article _article;
+  final ArticleType _type;
   bool _isBookmarked;
   int _selectedNavBar = 0;
   int _currentSliderIndex = 0;
+  ArticleDetailsResultBloc _bloc;
 
-  _DetailsPageState(this._article);
+  _DetailsPageState(this._article, this._type);
 
   @override
   void initState() {
     super.initState();
     _scrollViewController = ScrollController();
     _isBookmarked = false;
+    _bloc = ArticleDetailsResultBloc(
+        service: ReogAppsService.create(), type: _type);
   }
 
   @override
   void dispose() {
     super.dispose();
     _scrollViewController.dispose();
+    _bloc.close();
   }
 
   @override
   Widget build(BuildContext context) {
-    final _bottomNavBarItems = <BottomNavigationBarItem>[
-      BottomNavigationBarItem(
-          icon: (_isBookmarked)
-              ? Icon(Icons.bookmark)
-              : Icon(Icons.bookmark_border),
-          label:
-              (_isBookmarked) ? 'remove_bookmark'.tr() : 'add_bookmark'.tr()),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.comment),
-        label: 25.toString(),
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.remove_red_eye),
-        label: _article.views.toString(),
-      )
-    ];
-
-    final _bottomNavBar = BottomNavigationBar(
-      items: _bottomNavBarItems,
-      selectedItemColor: Color(0xff66A84D),
-      unselectedItemColor: Color(0xff66A84D),
-      currentIndex: _selectedNavBar,
-      onTap: onNavBarTapped,
+    final _bottomNavBar =
+        BlocBuilder<ArticleDetailsResultBloc, ArticleDetailsResultState>(
+      cubit: _bloc,
+      builder: (BuildContext context, ArticleDetailsResultState state) {
+        return BottomNavigationBar(
+          selectedItemColor: Color(0xff66A84D),
+          unselectedItemColor: Color(0xff66A84D),
+          currentIndex: _selectedNavBar,
+          onTap: onNavBarTapped,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: (_isBookmarked)
+                    ? Icon(Icons.bookmark)
+                    : Icon(Icons.bookmark_border),
+                label: (_isBookmarked)
+                    ? 'remove_bookmark'.tr()
+                    : 'add_bookmark'.tr()),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.comment),
+              label: 25.toString(),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.remove_red_eye),
+              label: (state is ArticleDetailsResultSuccessState)
+                  ? state.articleDetailsResult.data[0].views.toString()
+                  : "-",
+            )
+          ],
+        );
+      },
     );
 
     return WillPopScope(
@@ -71,98 +89,128 @@ class _DetailsPageState extends State<DetailsPage> {
         _navigateBack();
       },
       child: Scaffold(
-        body: NestedScrollView(
-          controller: _scrollViewController,
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                floating: false,
-                pinned: true,
-                snap: false,
-                forceElevated: innerBoxIsScrolled,
-                leading: IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  color: Colors.white,
-                  onPressed: () {
-                    _navigateBack();
-                  },
-                ),
-                actions: <Widget>[
-                  MainPopUpMenu(true),
-                ],
-                expandedHeight: 56.0 * 3.5,
-                flexibleSpace: FlexibleSpaceBar(
-                  title: Padding(
-                    padding: EdgeInsets.only(right: 6),
-                    child: Text(
-                      _article.title,
-                      maxLines: 4,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 13.0),
+        body: BlocBuilder<ArticleDetailsResultBloc, ArticleDetailsResultState>(
+          cubit: _bloc,
+          builder: (BuildContext context, ArticleDetailsResultState state) {
+            if (state is ArticleDetailsResultInitialState)
+              _bloc.add(ArticleDetailsResultFetching(id: _article.id));
+            return NestedScrollView(
+              controller: _scrollViewController,
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) {
+                return <Widget>[
+                  SliverAppBar(
+                    floating: false,
+                    pinned: true,
+                    snap: false,
+                    forceElevated: innerBoxIsScrolled,
+                    leading: IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      color: Colors.white,
+                      onPressed: () {
+                        _navigateBack();
+                      },
                     ),
-                  ),
-                  titlePadding: EdgeInsetsDirectional.only(
-                      start: 50, end: 16, bottom: 16),
-                  background: Container(
-                    color: Colors.black,
-                    child: Stack(
-                      children: <Widget>[
-                        CarouselSlider(
-                          items: _article.images.map((image) {
-                            return Container(
-                              width: MediaQuery.of(context).size.width,
-                              height: MediaQuery.of(context).size.height,
-                              child: Image.network(
-                                image.image,
-                                fit: BoxFit.cover,
-                              ),
-                            );
-                          }).toList(),
-                          options: CarouselOptions(
-                              autoPlay: true,
-                              height: MediaQuery.of(context).size.height,
-                              enableInfiniteScroll: false,
-                              autoPlayInterval: Duration(seconds: 5),
-                              onPageChanged: (index, reason) {
-                                setState(() {
-                                  _currentSliderIndex = index;
-                                });
-                              }),
+                    actions: <Widget>[
+                      MainPopUpMenu(true),
+                    ],
+                    expandedHeight: 56.0 * 3.5,
+                    flexibleSpace: FlexibleSpaceBar(
+                      title: Padding(
+                        padding: EdgeInsets.only(right: 6),
+                        child: Text(
+                          (state is ArticleDetailsResultSuccessState)
+                              ? state.articleDetailsResult.data[0].title
+                              : "",
+                          maxLines: 4,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 13.0),
                         ),
-                        Align(
-                          alignment: Alignment(0, 0.9),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: _article.images.map((image) {
-                              int index = _article.images.indexOf(image);
-                              return Container(
-                                width: 8,
-                                height: 8,
-                                margin: EdgeInsets.symmetric(horizontal: 2),
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: _currentSliderIndex == index
-                                      ? Color.fromRGBO(255, 255, 255, 0.9)
-                                      : Color.fromRGBO(255, 255, 255, 0.4),
-                                ),
-                              );
-                            }).toList(),
-                          ),
-                        )
-                      ],
+                      ),
+                      titlePadding: EdgeInsetsDirectional.only(
+                          start: 50, end: 16, bottom: 16),
+                      background: Container(
+                        color: Colors.black,
+                        child: Stack(
+                          children: <Widget>[
+                            CarouselSlider(
+                              items: (state is ArticleDetailsResultSuccessState)
+                                  ? _article.images.map((image) {
+                                      return Container(
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        height:
+                                            MediaQuery.of(context).size.height,
+                                        child: Image.network(
+                                          image.image,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      );
+                                    }).toList()
+                                  : [
+                                      Container(
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        height:
+                                            MediaQuery.of(context).size.height,
+                                      )
+                                    ],
+                              options: CarouselOptions(
+                                  autoPlay: true,
+                                  height: MediaQuery.of(context).size.height,
+                                  enableInfiniteScroll: false,
+                                  autoPlayInterval: Duration(seconds: 5),
+                                  onPageChanged: (index, reason) {
+                                    setState(() {
+                                      _currentSliderIndex = index;
+                                    });
+                                  }),
+                            ),
+                            Align(
+                              alignment: Alignment(0, 0.9),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: _article.images.map((image) {
+                                  int index = _article.images.indexOf(image);
+                                  return Container(
+                                    width: 8,
+                                    height: 8,
+                                    margin: EdgeInsets.symmetric(horizontal: 2),
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: _currentSliderIndex == index
+                                          ? Color.fromRGBO(255, 255, 255, 0.9)
+                                          : Color.fromRGBO(255, 255, 255, 0.4),
+                                    ),
+                                  );
+                                }).toList(),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ];
+              },
+              body: SingleChildScrollView(
+                padding: EdgeInsets.all(8),
+                child: (state is ArticleDetailsResultSuccessState)
+                    ? Text(
+                        state.articleDetailsResult.data[0].description,
+                        textAlign: TextAlign.justify,
+                      )
+                    : Container(
+                        child: Center(
+                          child: (state is ArticleDetailsResultLoadingState)
+                              ? CircularProgressIndicator()
+                              : Text((state as ArticleDetailsResultErrorState)
+                                  .error),
+                        ),
+                      ),
               ),
-            ];
+            );
           },
-          body: SingleChildScrollView(
-            padding: EdgeInsets.all(8),
-            child: Text(
-              _article.description,
-              textAlign: TextAlign.justify,
-            ),
-          ),
         ),
         bottomNavigationBar: _bottomNavBar,
       ),
