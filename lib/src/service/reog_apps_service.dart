0532515710cf03afe3e00c8d884cbfae37a8fd1c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-29 10:05:06 +0700

    Add an endpoint to get a news details

--- lib/src/service/reog_apps_service.dart
+++ lib/src/service/reog_apps_service.dart
@@ -1,4 +1,5 @@
 import 'package:chopper/chopper.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:reog_apps_flutter/src/service/header_interceptor.dart';
 
@@ -9,6 +10,9 @@ abstract class ReogAppsService extends ChopperService {
   @Get(path: 'api/news')
   Future<Response> getNews(
       {@Query('page') int page = 1, @Query('limit') int limit = 10});
+
+  @Get(path: 'api/news/{id}')
+  Future<Response> getNewsDetails(@Path('id') String id);
 
   static ReogAppsService create({String authTokenValue = ""}) {
     final client = ChopperClient(
