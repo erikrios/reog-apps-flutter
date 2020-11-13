9c1ed30e84f6937e2a3a95afc188781f2f11ff01
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-13 20:05:59 +0700

    Create foods_result_state.dart

---
+++ lib/src/bloc/states/foods_result_state.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+import 'package:reog_apps_flutter/src/models/articles_result.dart';
+
+abstract class FoodsResultState extends Equatable {
+  const FoodsResultState();
+}
+
+class FoodsResultInitialState extends FoodsResultState {
+  @override
+  List<Object> get props => [];
+}
+
+class FoodsResultLoadingState extends FoodsResultState {
+  @override
+  List<Object> get props => [];
+}
+
+class FoodsResultLoadingMoreState extends FoodsResultState {
+  @override
+  List<Object> get props => [];
+}
+
+class FoodsResultSuccessStata extends FoodsResultState {
+  final ArticlesResult foodsResult;
+
+  FoodsResultSuccessStata({this.foodsResult});
+
+  @override
+  List<Object> get props => [foodsResult];
+}
+
+class FoodsResultErrorState extends FoodsResultState {
+  final String error;
+
+  FoodsResultErrorState({this.error});
+
+  @override
+  List<Object> get props => [error];
+}
