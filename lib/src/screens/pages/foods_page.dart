27a3a710b2ce3a3fddfb2f8288ba1ce7893ff00b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-13 21:02:37 +0700

    Implements bloc to foods page

--- lib/src/screens/pages/foods_page.dart
+++ lib/src/screens/pages/foods_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
+import 'package:reog_apps_flutter/src/bloc/events/foods_result_event.dart';
+import 'package:reog_apps_flutter/src/bloc/foods_result_bloc.dart';
+import 'package:reog_apps_flutter/src/bloc/states/foods_result_state.dart';
 import 'package:reog_apps_flutter/src/models/article.dart';
+import 'package:reog_apps_flutter/src/models/articles.dart';
 import 'package:reog_apps_flutter/src/screens/pages/details_page.dart';
 import 'package:reog_apps_flutter/src/screens/widgets/food_item.dart';
 import 'package:reog_apps_flutter/src/utils/article_type.dart';
@@ -10,35 +16,146 @@ class FoodsPage extends StatefulWidget {
 }
 
 class _FoodsPageState extends State<FoodsPage> {
-  List<Article> _articles;
+  FoodsResultBloc _bloc;
+  int _currentPage = 1;
+  int _totalPage = 1;
+  final int _limit = 5;
+  String status;
+  Articles foods;
+
+  @override
+  void initState() {
+    super.initState();
+    _bloc = BlocProvider.of<FoodsResultBloc>(context);
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (_articles == null) {
-      _articles = List<Article>();
+    return _buildBlocBuilder(_bloc);
+  }
+
+  Widget _buildBlocBuilder(FoodsResultBloc bloc) {
+    return BlocBuilder<FoodsResultBloc, FoodsResultState>(
+      cubit: bloc,
+      builder: (BuildContext context, FoodsResultState state) {
+        if (state is FoodsResultInitialState) {
+          bloc.add(FoodsResultFetching(page: _currentPage, limit: _limit));
+          return Container();
+        } else if (state is FoodsResultLoadingState) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is FoodsResultErrorState) {
+          return RefreshIndicator(
+            onRefresh: () {
+              bloc.add(FoodsResultFetching(page: 1, limit: _limit));
+              return;
+            },
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                child: Center(
+                  child: Text(state.error),
+                ),
+              ),
+            ),
+          );
+        } else {
+          return _buildFoodsResult(state);
+        }
+      },
+    );
+  }
+
+  Widget _buildFoodsResult(FoodsResultState state) {
+    if (state is FoodsResultSuccessState) {
+      status = state.foodsResult.status;
+      foods = state.foodsResult.data[0];
+      _currentPage = state.foodsResult.data[0].currentPage;
+      _totalPage = state.foodsResult.data[0].totalPages;
     }
 
     return Container(
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
-        padding: EdgeInsets.all(3.0),
-        itemCount: _articles.length,
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            child: FoodItem(
-                "_articles[index].images[0]", "_articles[index].title"),
-            onTap: () async {
-              bool result = await Navigator.push(context,
-                  MaterialPageRoute(builder: (context) {
-                return DetailsPage(_articles[index], ArticleType.foods);
-              }));
-              print(result);
-            },
-          );
-        },
-      ),
-    );
+        child: status.toLowerCase() == 'error'
+            ? RefreshIndicator(
+                onRefresh: () {
+                  _bloc.add(FoodsResultFetching(page: 1, limit: _limit));
+                  return;
+                },
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height,
+                    child: Center(
+                      child: Text((state as FoodsResultSuccessState)
+                          .foodsResult
+                          .message),
+                    ),
+                  ),
+                ),
+              )
+            : foods.articles.isEmpty
+                ? Center(
+                    child: Text('Foods is empty.'),
+                  )
+                : LazyLoadScrollView(
+                    onEndOfPage: _loadMoreData,
+                    child: RefreshIndicator(
+                      onRefresh: () {
+                        _bloc.add(FoodsResultFetching(page: 1, limit: _limit));
+                        return;
+                      },
+                      child: ListView(
+                        padding: EdgeInsets.all(0),
+                        children: [
+                          GridView.builder(
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                            ),
+                            padding: EdgeInsets.all(3.0),
+                            itemCount: foods.articles.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return InkWell(
+                                child: FoodItem(
+                                    foods.articles[index].images.isEmpty
+                                        ? ""
+                                        : foods.articles[index].images[0].image,
+                                    foods.articles[index].title ?? ""),
+                                onTap: () {
+                                  _navigateToDetails(
+                                      context: context,
+                                      article: foods.articles[index]);
+                                },
+                              );
+                            },
+                          ),
+                          (state is FoodsResultLoadingMoreState)
+                              ? Center(
+                                  child: CircularProgressIndicator(),
+                                )
+                              : SizedBox(),
+                        ],
+                      ),
+                    ),
+                  ));
+  }
+
+  void _loadMoreData() {
+    if (_currentPage < _totalPage) {
+      _currentPage++;
+      _bloc.add(FoodsResultFetchingMore(page: _currentPage, limit: _limit));
+    }
+  }
+
+  void _navigateToDetails(
+      {@required BuildContext context, Article article}) async {
+    bool result =
+        await Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return DetailsPage(article, ArticleType.foods);
+    }));
+
+    print(result);
   }
 }
