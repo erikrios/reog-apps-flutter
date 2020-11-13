288f1f562e55ba71b26ad337bfbaa15027f6c67c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-13 20:19:51 +0700

    Create foods_result_bloc.dart

---
+++ lib/src/bloc/foods_result_bloc.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+import 'package:chopper/chopper.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reog_apps_flutter/src/bloc/events/foods_result_event.dart';
+import 'package:reog_apps_flutter/src/bloc/states/foods_result_state.dart';
+import 'package:reog_apps_flutter/src/models/article.dart';
+import 'package:reog_apps_flutter/src/models/articles_result.dart';
+import 'package:reog_apps_flutter/src/service/reog_apps_service.dart';
+
+class FoodsResultBloc extends Bloc<FoodsResultEvent, FoodsResultState> {
+  final ReogAppsService service;
+  final List<Article> foods = [];
+
+  FoodsResultBloc({@required this.service})
+      : assert(service != null),
+        super(FoodsResultInitialState());
+
+  @override
+  Stream<FoodsResultState> mapEventToState(FoodsResultEvent event) async* {
+    if (event is FoodsResultFetching) {
+      yield FoodsResultLoadingState();
+      try {
+        Response response =
+            await service.getFoods(page: event.page, limit: event.limit);
+        final foodsResult = ArticlesResult.fromJson(jsonDecode(response.body));
+        foods.clear();
+        foods.addAll(foodsResult.data[0].articles);
+        yield FoodsResultSuccessState(foodsResult: foodsResult);
+      } catch (e) {
+        yield FoodsResultErrorState(error: e.toString());
+      }
+    } else if (event is FoodsResultFetchingMore) {
+      yield FoodsResultLoadingMoreState();
+      try {
+        Response response =
+            await service.getFoods(page: event.page, limit: event.limit);
+        final foodsResult = ArticlesResult.fromJson(jsonDecode(response.body));
+        foods.addAll(foodsResult.data[0].articles);
+        foodsResult.data[0].articles.clear();
+        foodsResult.data[0].articles.addAll(foods);
+        yield FoodsResultSuccessState(foodsResult: foodsResult);
+      } catch (e) {
+        yield FoodsResultErrorState(error: e.toString());
+      }
+    }
+  }
+}
