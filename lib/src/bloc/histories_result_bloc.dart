7a9ea2c1e42a181fa8d49ddd50b17649783b3b51
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-14 10:12:22 +0700

    Create histories_result_bloc.dart

---
+++ lib/src/bloc/histories_result_bloc.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:chopper/chopper.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reog_apps_flutter/src/bloc/events/histories_result_event.dart';
+import 'package:reog_apps_flutter/src/bloc/states/histories_result_state.dart';
+import 'package:reog_apps_flutter/src/models/article.dart';
+import 'package:reog_apps_flutter/src/models/articles_result.dart';
+import 'package:reog_apps_flutter/src/service/reog_apps_service.dart';
+
+class HistoriesResultBloc
+    extends Bloc<HistoriesResultEvent, HistoriesResultState> {
+  final ReogAppsService service;
+  final List<Article> histories = [];
+
+  HistoriesResultBloc({@required this.service})
+      : assert(service != null),
+        super(HistoriesResultInitialState());
+
+  @override
+  Stream<HistoriesResultState> mapEventToState(
+      HistoriesResultEvent event) async* {
+    if (event is HistoriesResultFetching) {
+      yield HistoriesResultLoadingState();
+      try {
+        Response response =
+            await service.getHistories(page: event.page, limit: event.limit);
+        final historiesResult =
+            ArticlesResult.fromJson(jsonDecode(response.body));
+        histories.clear();
+        histories.addAll(historiesResult.data[0].articles);
+        yield HistoriesResultSuccessState(historiesResult: historiesResult);
+      } catch (e) {
+        yield HistoriesResultErrorSState(error: e.toString());
+      }
+    } else if (event is HistoriesResultFetchingMore) {
+      yield HistoriesResultLoadingMoreState();
+      try {
+        Response response =
+            await service.getHistories(page: event.page, limit: event.limit);
+        final historiesResult =
+            ArticlesResult.fromJson(jsonDecode(response.body));
+        histories.addAll(historiesResult.data[0].articles);
+        historiesResult.data[0].articles.clear();
+        historiesResult.data[0].articles.addAll(histories);
+        yield HistoriesResultSuccessState(historiesResult: historiesResult);
+      } catch (e) {
+        yield HistoriesResultErrorSState(error: e.toString());
+      }
+    }
+  }
+}
