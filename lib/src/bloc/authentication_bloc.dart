d1de58d2bd86ffc500068b12e31a2efd8633062b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-17 19:47:35 +0700

    Create authentication_bloc.dart

---
+++ lib/src/bloc/authentication_bloc.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:chopper/chopper.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reog_apps_flutter/src/bloc/events/auth_event.dart';
+import 'package:reog_apps_flutter/src/bloc/states/auth_result_state.dart';
+import 'package:reog_apps_flutter/src/models/auth_result.dart';
+import 'package:reog_apps_flutter/src/service/reog_apps_service.dart';
+
+class AuthenticationBloc extends Bloc<AuthEvent, AuthResultState> {
+  final ReogAppsService service;
+
+  AuthenticationBloc({@required this.service})
+      : assert(service != null),
+        super(AuthResultInitialState());
+
+  @override
+  Stream<AuthResultState> mapEventToState(AuthEvent event) async* {
+    if (event is Authenticating) {
+      yield AuthResultLoadingState();
+      try {
+        Response response = await service.authenticate(event.auth.toJson());
+        final authResult = AuthResult.fromJson(jsonDecode(response.body));
+        if (response.isSuccessful) {
+          yield AuthResultSuccessState(authResult: authResult);
+        } else
+          yield AuthResultErrorState(error: authResult.message);
+      } catch (e) {
+        yield AuthResultErrorState(error: e.toString());
+      }
+    }
+  }
+}
