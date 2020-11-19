519997ff03e83af7cf1dbfb4f996d5828bbe235c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-19 20:37:12 +0700

    Create avatar_result_state.dart

---
+++ lib/src/bloc/states/avatar_result_state.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:reog_apps_flutter/src/models/avatar_result.dart';
+
+abstract class AvatarResultState extends Equatable {
+  const AvatarResultState();
+}
+
+class AvatarResultLoadingState extends AvatarResultState {
+  @override
+  List<Object> get props => [];
+}
+
+class AvatarResultSuccessState extends AvatarResultState {
+  final AvatarResult avatarResult;
+
+  const AvatarResultSuccessState({this.avatarResult});
+
+  @override
+  List<Object> get props => [avatarResult];
+}
+
+class AvatarResultErrorState extends AvatarResultState {
+  final String error;
+
+  const AvatarResultErrorState({this.error});
+
+  @override
+  List<Object> get props => [props];
+}
