4eb72b576af51b63153a5ac7971cb0aa804af790
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-19 20:05:46 +0700

    Implement the bloc builder

--- lib/src/screens/pages/my_profile_page.dart
+++ lib/src/screens/pages/my_profile_page.dart
@@ -1,7 +1,19 @@
-import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reog_apps_flutter/src/bloc/events/user_result_event.dart';
+import 'package:reog_apps_flutter/src/bloc/states/user_result_state.dart';
+import 'package:reog_apps_flutter/src/bloc/user_result_bloc.dart';
+import 'package:reog_apps_flutter/src/service/reog_apps_service.dart';
 
 class MyProfilePage extends StatelessWidget {
+  final UserResultBloc _bloc =
+      UserResultBloc(service: ReogAppsService.create());
+
+  MyProfilePage() {
+    _bloc.add(UserResultFetching());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,39 +29,73 @@ class MyProfilePage extends StatelessWidget {
       ),
       body: Container(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 120,
-                height: 120,
-                child: CircleAvatar(
-                  backgroundColor: Colors.brown[300],
-                  backgroundImage: NetworkImage(
-                    'https://archive.org/download/shimla_phoenix_K011P-01c_panelscan/Fromental_Conversational/F006-willow-with-embroidery-col-custom.jpg',
-                  ),
-                ),
-              ),
-              SizedBox(height: 28),
-              Container(
-                alignment: Alignment.centerLeft,
-                margin: EdgeInsets.only(left: 58),
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    textItem('name'.tr(), 'Erik Rio Setiawan'),
-                    textItem(
-                        'email_address'.tr(), 'erikriosetiawan15@gmail.com'),
-                    textItem('member_since'.tr(), '2020/09/12'),
-                    textItem('last_login'.tr(), '2020/09/12'),
-                  ],
-                ),
-              ),
-            ],
+          child: BlocBuilder<UserResultBloc, UserResultState>(
+            cubit: _bloc,
+            builder: (BuildContext context, UserResultState state) {
+              if (state is UserResultInitialState)
+                return Container();
+              else if (state is UserResultLoadingState)
+                return _buildLoading();
+              else if (state is UserResultErrorState)
+                return _buildError(bloc: _bloc, state: state);
+              else
+                return _buildUserResult(context: context, state: state);
+            },
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildLoading() {
+    return CircularProgressIndicator();
+  }
+
+  Widget _buildError({UserResultBloc bloc, UserResultErrorState state}) {
+    return RefreshIndicator(
+      onRefresh: () {
+        bloc.add(UserResultFetching());
+        return;
+      },
+      child: Container(
+        child: Center(
+          child: Text(state.error),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserResult(
+      {BuildContext context, UserResultSuccessState state}) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 120,
+          height: 120,
+          child: CircleAvatar(
+            backgroundColor: Colors.brown[300],
+            backgroundImage: NetworkImage(
+              'https://archive.org/download/shimla_phoenix_K011P-01c_panelscan/Fromental_Conversational/F006-willow-with-embroidery-col-custom.jpg',
+            ),
+          ),
+        ),
+        SizedBox(height: 28),
+        Container(
+          alignment: Alignment.centerLeft,
+          margin: EdgeInsets.only(left: 58),
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              textItem('name'.tr(), 'Erik Rio Setiawan'),
+              textItem('email_address'.tr(), 'erikriosetiawan15@gmail.com'),
+              textItem('member_since'.tr(), '2020/09/12'),
+              textItem('last_login'.tr(), '2020/09/12'),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
