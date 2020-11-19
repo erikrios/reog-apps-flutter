566a16aaffd242db22ec958b8df0e15c81dc6d33
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-19 19:17:12 +0700

    Add a constant functions to get shared preferences

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -1,4 +1,16 @@
 library constants;
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 const String DARK_MODE_SHARED_PREFS_KEY = 'dark-mode-key';
 const String AUTH_TOKEN_SHARED_PREFS_KEY = 'auth-token-key';
+
+getAuthToken() async {
+  try {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    String token = preferences.getString(AUTH_TOKEN_SHARED_PREFS_KEY);
+    return token;
+  } catch (e) {
+    throw e;
+  }
+}
