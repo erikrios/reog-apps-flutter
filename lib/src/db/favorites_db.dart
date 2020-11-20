8bc95d55d26d9b53ae52c8fafe9cd950af3ec1fa
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-20 19:53:20 +0700

    Create the singleton object of favorites box

--- lib/src/db/favorites_db.dart
+++ lib/src/db/favorites_db.dart
@@ -4,10 +4,17 @@ import 'package:reog_apps_flutter/src/models/article.dart';
 import 'package:reog_apps_flutter/src/models/image.dart';
 
 class FavoritesDb {
+
+  static Box favoritesBox;
+
   static Future<void> init() async {
     var dir = await getApplicationDocumentsDirectory();
     Hive.init(dir.path);
     _registerAdapter();
+
+    if (favoritesBox == null) {
+      favoritesBox = await Hive.openBox('favorites');
+    }
   }
 
   static void _registerAdapter() {
