83a704ffcbdc71cca0bd84b5312977063d267c85
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-20 20:53:25 +0700

    Add some CRUD for the database

--- lib/src/db/favorites_db.dart
+++ lib/src/db/favorites_db.dart
@@ -4,8 +4,8 @@ import 'package:reog_apps_flutter/src/models/article.dart';
 import 'package:reog_apps_flutter/src/models/image.dart';
 
 class FavoritesDb {
-
   static Box favoritesBox;
+  static const _boxNameKey = 'favorites';
 
   static Future<void> init() async {
     var dir = await getApplicationDocumentsDirectory();
@@ -13,12 +13,42 @@ class FavoritesDb {
     _registerAdapter();
 
     if (favoritesBox == null) {
-      favoritesBox = await Hive.openBox('favorites');
+      favoritesBox = await Hive.openBox(_boxNameKey);
     }
   }
 
   static void _registerAdapter() {
     Hive.registerAdapter(ImageAdapter());
     Hive.registerAdapter(ArticleAdapter());
+  }
+
+  static void addFavoriteArticle(Article article) {
+    openBox();
+    favoritesBox.put(article.id, article);
+    favoritesBox.close();
+  }
+
+  static Article getFavoriteArticleById(String id) {
+    openBox();
+    Article article = favoritesBox.get(id);
+    favoritesBox.close();
+    return article;
+  }
+
+  static List<Article> getFavoriteArticles() {
+    openBox();
+    List<Article> articles = favoritesBox.toMap().values.toList();
+    favoritesBox.close();
+    return articles;
+  }
+
+  static void deleteArticleById(Article article) {
+    openBox();
+    article.delete();
+    favoritesBox.close();
+  }
+
+  static void openBox() async {
+    if (!favoritesBox.isOpen) favoritesBox = await Hive.openBox(_boxNameKey);
   }
 }
