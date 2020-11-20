994309fccd916f7b24382b57afdb88853a6ed5ff
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-20 19:42:07 +0700

    Add a static method to initialize hive

--- lib/src/db/favorites_db.dart
+++ lib/src/db/favorites_db.dart
@@ -1,9 +1,16 @@
 import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:reog_apps_flutter/src/models/article.dart';
 import 'package:reog_apps_flutter/src/models/image.dart';
 
 class FavoritesDb {
-  static void registerAdapter() {
+  static Future<void> init() async {
+    var dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+    _registerAdapter();
+  }
+
+  static void _registerAdapter() {
     Hive.registerAdapter(ImageAdapter());
     Hive.registerAdapter(ArticleAdapter());
   }
