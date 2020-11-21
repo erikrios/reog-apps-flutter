f1c889dac42c7a6041753e1eb4a4199e500b4ddc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-21 11:21:11 +0700

    Register the adapter

--- lib/src/db/favorites_db.dart
+++ lib/src/db/favorites_db.dart
@@ -2,10 +2,13 @@ import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:reog_apps_flutter/src/models/article.dart';
 import 'package:reog_apps_flutter/src/models/image.dart';
+import 'package:reog_apps_flutter/src/utils/article_type.dart';
 
 class FavoritesDb {
   static Box _favoritesBox;
+  static Box _articleTypeBox;
   static const _boxNameKey = 'favorites';
+  static const _articleTypeBoxNameKey = 'article-type';
 
   static Future<void> init() async {
     var dir = await getApplicationDocumentsDirectory();
@@ -15,53 +18,60 @@ class FavoritesDb {
     if (_favoritesBox == null) {
       _favoritesBox = await Hive.openBox(_boxNameKey);
     }
+
+    if (_articleTypeBox == null) {
+      _articleTypeBox = await Hive.openBox(_articleTypeBoxNameKey);
+    }
   }
 
   static void _registerAdapter() {
     Hive.registerAdapter(ImageAdapter());
     Hive.registerAdapter(ArticleAdapter());
+    Hive.registerAdapter(ArticleTypeAdapter());
   }
 
   static void addFavoriteArticle(Article article) {
-    openBox();
+    _openBox();
     _favoritesBox.put(article.id, article);
     _favoritesBox.close();
   }
 
   static Article getFavoriteArticleById(String id) {
-    openBox();
+    _openBox();
     Article article = _favoritesBox.get(id);
     _favoritesBox.close();
     return article;
   }
 
   static List<Article> getFavoriteArticles() {
-    openBox();
+    _openBox();
     List<Article> articles = _favoritesBox.toMap().values.toList();
     _favoritesBox.close();
     return articles;
   }
 
   static void deleteFavoriteArticle(Article article) {
-    openBox();
+    _openBox();
     article.delete();
     _favoritesBox.close();
   }
 
   static void deleteFavoriteArticleById(String id) {
-    openBox();
+    _openBox();
     _favoritesBox.delete(id);
     _favoritesBox.close();
   }
 
   static bool isFavoriteArticleExists(String id) {
-    openBox();
+    _openBox();
     bool isExists = _favoritesBox.containsKey(id);
     _favoritesBox.close();
     return isExists;
   }
 
-  static void openBox() async {
+  static void _openBox() async {
     if (!_favoritesBox.isOpen) _favoritesBox = await Hive.openBox(_boxNameKey);
+    if (!_articleTypeBox.isOpen)
+      _articleTypeBox = await Hive.openBox(_articleTypeBoxNameKey);
   }
 }
