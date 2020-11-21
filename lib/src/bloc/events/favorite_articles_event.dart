d37e7c56c6f551d526fcfc3c3cd7ddae10505773
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-21 11:50:50 +0700

    Create favorite_articles_event.dart

---
+++ lib/src/bloc/events/favorite_articles_event.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+class FavoriteArticlesEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class GetFavoriteArticlesEvent extends FavoriteArticlesEvent {}
+
+class DeleteFavoriteArticleEvent extends FavoriteArticlesEvent {}
