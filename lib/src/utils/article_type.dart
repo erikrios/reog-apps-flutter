b86e52b5fe390b6e9cfebb1db5b8f8dc8ef90407
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-21 11:20:25 +0700

    Add Hive type converter annotation

--- lib/src/utils/article_type.dart
+++ lib/src/utils/article_type.dart
@@ -1,1 +1,15 @@
-enum ArticleType { news, foods, sites, history }
+import 'package:hive/hive.dart';
+
+part 'article_type.g.dart';
+
+@HiveType(typeId: 3)
+enum ArticleType {
+  @HiveField(0)
+  news,
+  @HiveField(1)
+  foods,
+  @HiveField(2)
+  sites,
+  @HiveField(3)
+  history,
+}
