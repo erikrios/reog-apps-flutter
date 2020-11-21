f322cb594c85a11536bdeefebc0ee7563118eee6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-21 12:53:48 +0700

    Implements bloc builder

--- lib/src/screens/pages/favorites_page.dart
+++ lib/src/screens/pages/favorites_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reog_apps_flutter/src/bloc/events/favorite_articles_event.dart';
 import 'package:reog_apps_flutter/src/bloc/favorite_articles_bloc.dart';
 import 'package:reog_apps_flutter/src/bloc/states/favorite_articles_state.dart';
@@ -38,36 +39,56 @@ class _FavoritesPageState extends State<FavoritesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: NestedScrollView(
-          controller: _scrollViewController,
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                title: Text('Favorites'),
-                floating: true,
-                snap: true,
-                forceElevated: innerBoxIsScrolled,
-                actions: [
-                  BrightnessMenu(),
-                  MainPopUpMenu(true),
-                ],
-              )
-            ];
+        controller: _scrollViewController,
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              title: Text('Favorites'),
+              floating: true,
+              snap: true,
+              forceElevated: innerBoxIsScrolled,
+              actions: [
+                BrightnessMenu(),
+                MainPopUpMenu(true),
+              ],
+            )
+          ];
+        },
+        body: BlocBuilder<FavoriteArticlesBloc, FavoriteArticlesState>(
+          cubit: _bloc,
+          builder: (BuildContext context, FavoriteArticlesState state) {
+            if (state is FavoriteArticlesLoadingState)
+              return _buildLoading()
+            else
+              return _buildFavoriteArticles(
+                  state as FavoriteArticlesSuccessState, _bloc);
           },
-          body:
+        ),
       ),
     );
   }
 
   Widget _buildLoading() {
-    return Container(child: Center(child: CircularProgressIndicator(),),);
+    return Container(
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 
   Widget _buildFavoriteArticles(FavoriteArticlesSuccessState state,
       FavoriteArticlesBloc bloc) {
     if (state is FavoriteArticlesSuccessEmptyState) {
-      return Container(child: Center(child: Text(state.message,),),);
+      return Container(
+        child: Center(
+          child: Text(
+            state.message,
+          ),
+        ),
+      );
     } else {
-      FavoriteArticlesSuccessNotEmptyState notEmptyState = state as FavoriteArticlesSuccessNotEmptyState;
+      FavoriteArticlesSuccessNotEmptyState notEmptyState =
+      state as FavoriteArticlesSuccessNotEmptyState;
       List<Article> articles = notEmptyState.articles;
       return ListView.builder(
           shrinkWrap: true,
@@ -75,15 +96,21 @@ class _FavoritesPageState extends State<FavoritesPage> {
           itemCount: articles.length,
           itemBuilder: (BuildContext context, int index) {
             return Dismissible(
-              background: Container(color: Colors.red,
-                child: Center(child: Text('Remove from favorites.',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.white),),),),
+              background: Container(
+                color: Colors.red,
+                child: Center(
+                  child: Text(
+                    'Remove from favorites.',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                ),
+              ),
               key: Key(articles[index].id),
               onDismissed: (DismissDirection direction) {
                 bloc.add(DeleteFavoriteArticleEvent());
-                Scaffold.of(context).showSnackBar(SnackBar(content: Text(
-                    '${articles[index].title} removed.')));
+                Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text('${articles[index].title} removed.')));
               },
               child: Material(
                 child: InkWell(
@@ -106,12 +133,13 @@ class _FavoritesPageState extends State<FavoritesPage> {
     }
   }
 
-  void _navigateToDetails(
-      {@required BuildContext context, @required Article article, @required ArticleType type}) async {
-    bool result = await Navigator.push(
-        context, MaterialPageRoute(builder: (BuildContext context) {
-      return DetailsPage(article, type);
-    }));
+  void _navigateToDetails({@required BuildContext context,
+    @required Article article,
+    @required ArticleType type}) async {
+    bool result = await Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) {
+          return DetailsPage(article, type);
+        }));
 
     print(result);
   }
