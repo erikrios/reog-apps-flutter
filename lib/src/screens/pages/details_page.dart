2922d57abfb93277c28318bbc23e085ef1f6064e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-22 21:05:45 +0700

    Refactor the UI code

--- lib/src/screens/pages/details_page.dart
+++ lib/src/screens/pages/details_page.dart
@@ -3,15 +3,17 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reog_apps_flutter/src/bloc/article_details_result_bloc.dart';
+import 'package:reog_apps_flutter/src/bloc/comment_bloc.dart';
 import 'package:reog_apps_flutter/src/bloc/events/article_details_result_event.dart';
 import 'package:reog_apps_flutter/src/bloc/states/article_details_result_state.dart';
+import 'package:reog_apps_flutter/src/bloc/states/comment_state.dart';
 import 'package:reog_apps_flutter/src/db/favorites_db.dart';
 import 'package:reog_apps_flutter/src/models/article.dart';
-import 'package:reog_apps_flutter/src/models/comment.dart';
 import 'package:reog_apps_flutter/src/screens/widgets/comment_item.dart';
 import 'package:reog_apps_flutter/src/screens/widgets/main_pop_up_menu.dart';
 import 'package:reog_apps_flutter/src/service/reog_apps_service.dart';
 import 'package:reog_apps_flutter/src/utils/article_type.dart';
+import 'package:reog_apps_flutter/src/utils/constants.dart';
 
 class DetailsPage extends StatefulWidget {
   final Article _article;
@@ -245,22 +247,6 @@ class _DetailsPageState extends State<DetailsPage> {
           break;
         case 1:
           {
-            List<Comment> comments = new List<Comment>();
-            for (int i = 0; i < 30; i++) {
-              if (i % 2 == 0)
-                comments.add(new Comment(
-                    'Author Name $i',
-                    '$i Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea',
-                    'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/800px-The_Earth_seen_from_Apollo_17.jpg',
-                    'Tue, $i Oct 20 08:50 pm'));
-              else
-                comments.add(new Comment(
-                    'Author Name $i',
-                    '$i Lorem ipsum dolor sit amet dolore magna aliqua. Sed do eiusmod tempor.',
-                    'https://archive.org/download/shimla_phoenix_K011P-01c_panelscan/Fromental_Conversational/F006-willow-with-embroidery-col-custom.jpg',
-                    'Tue, $i Oct 20 08:50 pm'));
-            }
-
             TextEditingController controller = TextEditingController();
 
             showModalBottomSheet(
@@ -269,64 +255,9 @@ class _DetailsPageState extends State<DetailsPage> {
                     borderRadius: BorderRadius.circular(10)),
                 context: context,
                 builder: (builder) {
-                  return Container(
-                    alignment: Alignment.topCenter,
-                    child: Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(top: 32),
-                          width: 100,
-                          height: 10,
-                          decoration: BoxDecoration(
-                            color: Colors.black12,
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                        ),
-                        Flexible(
-                          flex: 1,
-                          child: ListView.builder(
-                            itemBuilder: (context, index) {
-                              return CommentItem(
-                                  comments[index].author,
-                                  comments[index].comment,
-                                  comments[index].url,
-                                  comments[index].date);
-                            },
-                            itemCount: 30,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                              bottom: MediaQuery.of(context).viewInsets.bottom),
-                          child: TextField(
-                            controller: controller,
-                            keyboardType: TextInputType.multiline,
-                            minLines: 1,
-                            maxLines: 5,
-                            decoration: InputDecoration(
-                              suffixIcon: Padding(
-                                padding:
-                                    const EdgeInsetsDirectional.only(end: 8.0),
-                                child: IconButton(
-                                  icon: Icon(Icons.send),
-                                  onPressed: () {
-                                    print('Send');
-                                  },
-                                ),
-                              ),
-                              hintText: 'Type your comment...',
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide.none,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(0)),
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+                  return RefreshIndicator(
+                    onRefresh: null,
+                    child: null,
                   );
                 });
           }
@@ -335,6 +266,78 @@ class _DetailsPageState extends State<DetailsPage> {
           print('Show viewers');
       }
     });
+  }
+
+  Widget _buildSuccessComment(CommentBloc bloc, CommentSuccessState state,
+      TextEditingController controller) {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 32),
+            width: 100,
+            height: 10,
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: (state is CommentSuccessNotEmptyState)
+                ? ListView.builder(
+                    itemBuilder: (context, index) {
+                      return CommentItem(
+                          state.commentResult.data[index].user.name,
+                          state.commentResult.data[index].comment,
+                          state.commentResult.data[index].date);
+                    },
+                    itemCount: state.commentResult.data.length,
+                  )
+                : Container(
+                    child: Center(
+                      child: Text((state as CommentSuccessEmptyState).message),
+                    ),
+                  ),
+          ),
+          getAuthToken().then(
+            (value) {
+              (value != null)
+                  ? Padding(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom),
+                      child: TextField(
+                        controller: controller,
+                        keyboardType: TextInputType.multiline,
+                        minLines: 1,
+                        maxLines: 5,
+                        decoration: InputDecoration(
+                          suffixIcon: Padding(
+                            padding: const EdgeInsetsDirectional.only(end: 8.0),
+                            child: IconButton(
+                              icon: Icon(Icons.send),
+                              onPressed: () {
+                                print('Send');
+                              },
+                            ),
+                          ),
+                          hintText: 'Type your comment...',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.all(Radius.circular(0)),
+                          ),
+                        ),
+                      ),
+                    )
+                  : SizedBox();
+            },
+          )
+        ],
+      ),
+    );
   }
 
   void _navigateBack() {
