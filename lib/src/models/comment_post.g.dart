97ba3cf45e903640a653f23e476af011096bf6ae
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-22 19:37:47 +0700

    Generate comment_post.g.dart

---
+++ lib/src/models/comment_post.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'comment_post.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CommentPost _$CommentPostFromJson(Map<String, dynamic> json) {
+  return CommentPost(
+    comment: json['comment'] as String,
+  );
+}
+
+Map<String, dynamic> _$CommentPostToJson(CommentPost instance) =>
+    <String, dynamic>{
+      'comment': instance.comment,
+    };
