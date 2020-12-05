9a6b5656d85fe6b225f99e9d10bafb30c237b1c2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-05 21:23:27 +0700

    Check the user login

--- lib/src/screens/pages/favorites_page.dart
+++ lib/src/screens/pages/favorites_page.dart
@@ -11,6 +11,7 @@ import 'package:reog_apps_flutter/src/screens/widgets/article_item.dart';
 import 'package:reog_apps_flutter/src/screens/widgets/brightness_menu.dart';
 import 'package:reog_apps_flutter/src/screens/widgets/main_pop_up_menu.dart';
 import 'package:reog_apps_flutter/src/utils/article_type.dart';
+import 'package:reog_apps_flutter/src/utils/constants.dart';
 import 'package:startapp/startapp.dart';
 
 class FavoritesPage extends StatefulWidget {
@@ -65,7 +66,8 @@ class _FavoritesPageState extends State<FavoritesPage> {
                 ),
                 actions: [
                   BrightnessMenu(),
-                  MainPopUpMenu(true),
+                  MainPopUpMenu(getAuthToken()
+                      .then((value) => value == null ? false : true)),
                 ],
               )
             ];
